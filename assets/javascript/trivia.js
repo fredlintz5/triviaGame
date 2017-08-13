78e0123b8c404dd92bf5aa442e4659f59865c7b7
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-08-12 23:22:25 -0600

    updated js to work with timer, and added fun images

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,55 +1,62 @@
 $(document).ready(function() {
 	
-	var question0 = {
+	var question1 = {
+		text: "What is your Name?", 
+		answer: "Arthur, King of the Britons.",
+		wrong: ["Sir Galahad of Camelot.", "Sir Lancelot of Camelot.", "Sir Robin of Camelot."],
+		correct: false,
+	}
+
+	var question2 = {
 		text: "What is your Quest?", 
 		answer: "To seek the Holy Grail.",
 		wrong: ["To seek the Holey Grail.", "To seek the Whole Grail.", "To eat cheese and be merry."],
 		correct: false,
 	}
 
-	var question1 = {
+	var question4 = {
 		text: "What is your favorite color?", 
 		answer: "Blue",
 		wrong: ["Red", "Green", "Yellow."],
 		correct: false,
 	}
 
-	var question2 = {
+	var question3 = {
 		text: "What is the average air speed velocity of an unladen swallow?", 
 		answer: "Is that an African or European Swallow?",
 		wrong: ["46 mph", "10 mph", "Lightspeed"],
 		correct: false,
 	}
 
-	var questionBank = [question0, question1, question2];
+	var questionBank = [question1, question2, question3, question4];
 	var bankLength = questionBank.length;
 	var count = 0;
+	var intervalID; 
+	var time = 5;
 
 
 $("#start").click(function() {
 
 	createQuestions(questionBank[count]);
-	$("#gameboard").css('display', 'none');
+	$("#splashScreen").css('display', 'none');
 	$("#questions").css('display', 'inherit');
 
 });
 
 
 
-
 function createQuestions(array) {
 
-	$("#timer").css('display', 'inherit');
-	$("#text div").html("<div><h4>" + array.text);
-	$("#answers").html("<div><button data-correct='true'>" + array.answer);
+	intervalID = setInterval(timer, 1000);
+	$("#snarf").css('background', '#FFF');
+	$("#text").html("<div><h4>" + array.text);
+	$("#answers").html("<div class='text-center btn btn-info btn-block' data-correct='true'>" + array.answer);
 
-	for (var j = 0; j < array.wrong.length; j++) {
-		$("#answers").append("<div><button data-correct='false'>" + array.wrong[j]);
-	}
+	for (var i = 0; i < array.wrong.length; i++) {
+		$("#answers").append("<div class='text-center btn btn-info btn-block' data-correct='false'>" + array.wrong[i]);
+	};
 
-	fiveSecTimer();
 	correct();
-
 }
 
 
@@ -59,52 +66,91 @@ function nextQuestion() {
 
 
 function correct() {
-	$("button").click(function() {
+	$("#answers div").click(function() {
 
 		var questCorrect = $(this).data("correct");
 
 		if (questCorrect === true) {
 			$(this).css('background', '#5CB85C');
+			questionBank[count].correct = "Right. Off you go. [crosses the Bridge]";
 			count++;
-			nextQuestion();
+			clearInterval(intervalID);
+			time = 5;
+			setTimeout(function() {
+				checkGameEnd();		
+			}, 300);
 
 		} else {
 			$(this).css('background', '#D9534F');
+			questionBank[count].correct = "AAAAAAAAGHHHHH! [is cast into the gorge by an invisible force]";
+			count++;
+			clearInterval(intervalID);
+			time = 5;
+			setTimeout(function() {
+				checkGameEnd();		
+			}, 300);	
 		}
 
 	});
 }
 
 
-function fiveSecTimer() {
-	setTimeout(function(){
-		$("#timer").html("<h2>5</h2>");
-	}, 0000);
+function checkGameEnd() {
+	if (count === questionBank.length) {
+	$("#questions").css('display', 'none');	
+	createResults();
+	$("#gameOver").css('display', 'inherit');
 
-	setTimeout(function(){
-		$("#timer").html("<h2>4</h2>");
-	}, 1000);
-
-	setTimeout(function(){
-		$("#timer").html("<h2>3</h2>");
-	}, 2000);
-
-	setTimeout(function(){
-		$("#timer").html("<h2>2</h2>");
-	}, 3000);
-
-	setTimeout(function(){
-		$("#timer").html("<h2>1</h2>");
-	}, 4000);
-
-	setTimeout(function(){
-		$("#timer").html("<h2>0</h2>");
-	}, 5000);
-	
+	} else {
+		nextQuestion();
+	}
 }
 
+
+function createResults() {
+
+	for (var i = 0; i < bankLength; i++) {
+
+		$("#results").append("<div>Question #"+[i+1]+': ' + questionBank[i].correct + "</div>");
+	}
+}
+
+
+$("#restart").click(function() {
+
+	count = 0;
+	$("#results").empty();	
+
+	for (var i = 0; i < bankLength; i++) {
+		questionBank[i].correct = false;
+	}
+
+	$("#gameOver").css('display', 'none');
+	$("#splashScreen").css('display', 'inherit');
 
 });
 
 
+function timer() {
+	$("#timer h1").html("00:0"+time);
+	$("#timer").css('visibility', 'inherit');
 
+	if (time === 0) {
+
+		$("#snarf").css('background', '#D9534F');
+		clearInterval(intervalID);
+		time = 5;
+		questionBank[count].correct = "AAAAAAAAGHHHHH! [is cast into the gorge by an invisible force]";
+		count++;
+		setTimeout(function() {
+			checkGameEnd();		
+		}, 600);
+	}
+	time--;
+};
+
+
+
+
+
+});
